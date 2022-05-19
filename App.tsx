447337048006552d00461b126fc14847a27e1e38
janson000 <mail4jans@gmail.com>
2022-05-19 23:19:01 +0530

    4 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
+      <h1 id="Root">Root</h1>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
--- index.tsx
+++ index.tsx
@@ -4,6 +4,9 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
+
+
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
