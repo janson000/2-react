1dbb2a57ca10e5aedfd9ae2b5799787779ce17b3
janson000 <mail4jans@gmail.com>
2022-05-23 21:36:40 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -5,11 +5,6 @@ import { createRoot } from 'react-dom/client';
 //1
 import App from './App';
 
-//5
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
 //1
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -19,46 +14,3 @@ root.render(
     <App />
   </StrictMode>
 );
-
-//5
-
-function Car(props) {
-  return <h2>I am a {props.brand.model}!</h2>;
-}
-
-function Garage() {
-  const carInfo = { name: 'Ford', model: 'Mustang' };
-  return (
-    <>
-      <h1>Who lives in my garage?</h1>
-      <Car brand={carInfo} />
-    </>
-  );
-}
-
-const rootfive = ReactDOM.createRoot(document.getElementById('rootfive'));
-rootfive.render(<Garage />);
-
-//6
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-function Car(props) {
-  return <li>I am a { props.brand }</li>;
-}
-
-function Garage() {
-  const cars = ['Ford', 'BMW', 'Audi'];
-  return (
-    <>
-	    <h1>Who lives in my garage?</h1>
-	    <ul>
-        {cars.map((car) => <Car brand={car} />)}
-      </ul>
-    </>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Garage />);
-
