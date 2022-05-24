0aee2e0cee9fa9b33fb156202429254298c9335f
janson000 <mail4jans@gmail.com>
2022-05-24 09:24:37 +0530

    2 files modified, 1 added

--- index.tsx
+++ index.tsx
@@ -14,3 +14,88 @@ root.render(
     <App />
   </StrictMode>
 );
+
+//5
+
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function MyForm() {
+  const [textarea, setTextarea] = useState(
+    'The content of a textarea goes in the value attribute'
+  );
+
+  const handleChange = (event) => {
+    setTextarea(event.target.value);
+  };
+
+  return (
+    <form>
+      <textarea value={textarea} onChange={handleChange} />
+    </form>
+  );
+}
+
+const rootfive = ReactDOM.createrootfive(document.getElementById('rootfive'));
+rootfive.render(<MyForm />);
+
+//6
+
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function MyForm() {
+  const [name, setName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter your name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  );
+}
+
+const rootsix = ReactDOM.createRoot(document.getElementById('rootsix'));
+rootsix.render(<MyForm />);
+
+//7
+
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function MyForm() {
+  const [myCar, setMyCar] = useState('Volvo');
+
+  const handleChange = (event) => {
+    setMyCar(event.target.value);
+  };
+
+  return (
+    <form>
+      <select value={myCar} onChange={handleChange}>
+        <option value="Ford">Ford</option>
+        <option value="Volvo">Volvo</option>
+        <option value="Fiat">Fiat</option>
+      </select>
+    </form>
+  );
+}
+
+const rootseven = ReactDOM.createrootseven(
+  document.getElementById('rootseven')
+);
+rootseven.render(<MyForm />);
+
+//8
